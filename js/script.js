9a602e72612e63bf901857b0851006708fd439b6
Gakat _ <biaro.falcon@gmail.com>
2018-11-14 17:39:12 +0200

    Header and slider

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".slider").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: false,
+    nav: true
+  });
+  $("button.owl-prev span").replaceWith("<img src=\"img\\prev.png\\\" alt=\"\"></img>");
+  $("button.owl-next span").replaceWith("<img src=\"img\\next.png\\\" alt=\"\"></img>");
+});
